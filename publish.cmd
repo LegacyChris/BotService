1f05b81d46c2517de813b76d062230ca84de85f4
Christian Chime <legacchris@gmail.com>
2020-01-20 09:57:03 +0000

    Initail Commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=staff-time-management-bot-Web-Deploy.pubxml -p:Password=2YyoddbA13CB4bsvQPq93WhX3ikDi4hHxedhetZh6yzQGaDMD2uP88EkCf6R
+
